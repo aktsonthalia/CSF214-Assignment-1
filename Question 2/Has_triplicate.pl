82d2d3ce7037682cb8673a5dc3e01e42c670424f
red-rising <53793862+red-rising@users.noreply.github.com>
2019-10-14 01:24:55 +0530

    Update Has_triplicate.pl

--- Question 2/Has_triplicate.pl
+++ Question 2/Has_triplicate.pl
@@ -1,6 +1,26 @@
 :-style_check(-singleton).
 :- ['../Utils.pl'].
 
+% ###################################################################
+
+% has_triplicate/1 finds all elements that occur three or more times in the given list.
+
 has_triplicate([]) :- false.
-has_triplicate(X) :- remove_duplicates(X, [], X_without_repitition), 
-					 member(X_without_repitition, Element), occurences(X, Element, 0, N), N >= 3, write(Element).
+
+has_triplicate(X) :- 
+					 % removes duplicates from 'X' and stores the resultant list in 'X_without_repitition'.
+					 % This predicate ensures that no element is printed multiple times.
+					 remove_duplicates(X, [], X_without_repitition), 
+
+					 % 'Element' is a member of 'X_without_repitition'. 
+					 member(X_without_repitition, Element), 
+
+					 % 'N' is the number of times 'Element' occurs in 'X'.
+					 occurences(X, Element, 0, N), 
+
+					 % For 'Element' to be a triplicate, the following condition has to hold.
+					 N >= 3, 
+
+					 write(Element).
+					 
+% ###################################################################
